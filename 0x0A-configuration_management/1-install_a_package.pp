2fea5d2c3f9f33939544ea22eb20684ce07544c5
kendimuthomi <kendijackline42@gmail.com>
2022-12-10 01:37:46 +0000

    Install using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
 # install flask using puppet
 exec {'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+  command  => '/usr/bin/apt-get -y install flask -v 2.1.0',
+  provider => 'pip3',
 }
