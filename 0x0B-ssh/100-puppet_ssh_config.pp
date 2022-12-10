52e7eed484f393b0ffc72534c770ca2224408025
kendimuthomi <kendijackline42@gmail.com>
2022-12-10 16:47:41 +0000

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,5 @@
 # Config file using puppet
-include stdlib
 
-file_line { 'Turn off passwd auth':
-  ensure             => created,
-  path               => '/etc/ssh/ssh_config'
-  line               => '    PasswordAuthentication no',
-  replace            => true,
-  append_on_no_match => true
-}
-
-file_line { 'Declare identity file':
-  ensure             => created,
-  path               => '/etc/ssh/ssh_config',
-  line               => '     IdentityFile ~/.ssh/school',
-  replace            => true,
-  append_on_no_match => true
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+  path => '/bin/'
 }
