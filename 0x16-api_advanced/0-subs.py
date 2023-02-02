73e4fd0dc7fa029b7be9140de7b7f0efc1813171
kendimuthomi <kendijackline42@gmail.com>
2023-02-02 14:41:01 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'kendimuthomi'}
+    headers = {'User-Agent': 'Spam4Karma'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         return (res.json().get("data").get("subscribers"))
