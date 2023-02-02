c5f92b1cbcd6642780f07db020ce42191d2ce0a4
kendimuthomi <kendijackline42@gmail.com>
2023-02-02 14:53:38 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,14 @@
 total subscribers) for a given subreddit"""
 
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Spam4Karma'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
